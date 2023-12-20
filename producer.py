41a1b9fbf982836f66be0f973a4b2c8949d02ff4
Enes <59286704+enessskaragoz@users.noreply.github.com>
2023-12-20 14:59:32 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,95 +1,80 @@
-import os
-import json
-import tweepy
-from confluent_kafka import avro
-from confluent_kafka.avro import AvroProducer
+import com.google.gson.Gson;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
-# Twitter API credentials
-consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+import java.util.Properties;
+import java.util.Scanner;
 
-# Kafka topic and schema registry URL
-topic = 'tweets'
-schema_registry_url = 'http://localhost:8081'
+public class Producer {
 
-# Avro schema for tweet value
-value_schema_str = """
-{
-   "namespace": "twitter",
-   "name": "tweet",
-   "type": "record",
-   "fields" : [
-       {"name" : "id", "type" : "long"},
-       {"name" : "created_at", "type" : "string"},
-       {"name" : "text", "type" : "string"},
-       {"name" : "user_id", "type" : "long"},
-       {"name" : "user_name", "type" : "string"},
-       {"name" : "user_location", "type" : ["null", "string"]},
-       {"name" : "user_followers_count", "type" : "int"},
-       {"name" : "user_friends_count", "type" : "int"},
-       {"name" : "user_verified", "type" : "boolean"},
-       {"name" : "lang", "type" : "string"},
-       {"name" : "retweet_count", "type" : "int"},
-       {"name" : "favorite_count", "type" : "int"}
-   ]
+    public static void main(String[] args) {
+
+        // Kafka topic and bootstrap server
+        String topic = "messages";
+        String bootstrapServer = "localhost:9092";
+
+        // Create a Kafka producer
+        Properties props = new Properties();
+        props.put("bootstrap.servers", bootstrapServer);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        // Create a Gson object
+        Gson gson = new Gson();
+
+        // Create a scanner to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Loop to get user input and send messages to Kafka
+        while (true) {
+            // Prompt the user to enter a name and a message
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+            System.out.print("Enter your message: ");
+            String message = scanner.nextLine();
+
+            // Create a message object with name and message
+            Message msg = new Message(name, message);
+
+            // Convert the message object to JSON string
+            String json = gson.toJson(msg);
+
+            // Create a producer record with topic, key and value
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, name, json);
+
+            // Send the record to Kafka
+            producer.send(record);
+
+            // Print the record
+            System.out.println("Sent: " + record);
+        }
+    }
 }
-"""
 
-# Create an Avro producer
-value_schema = avro.loads(value_schema_str)
-avro_producer = AvroProducer({
-    'bootstrap.servers': 'localhost:9092',
-    'schema.registry.url': schema_registry_url
-    }, default_value_schema=value_schema)
+// A class to represent a message
+class Message {
+    private String name;
+    private String message;
 
-# Create a tweepy stream listener
-class TweetListener(tweepy.StreamListener):
+    public Message(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
 
-    def on_data(self, data):
-        try:
-            # Decode the JSON data
-            tweet = json.loads(data)
+    public String getName() {
+        return name;
+    }
 
-            # Extract relevant information from the tweet
-            value = {
-                "id": tweet["id"],
-                "created_at": tweet["created_at"],
-                "text": tweet["text"],
-                "user_id": tweet["user"]["id"],
-                "user_name": tweet["user"]["name"],
-                "user_location": tweet["user"]["location"],
-                "user_followers_count": tweet["user"]["followers_count"],
-                "user_friends_count": tweet["user"]["friends_count"],
-                "user_verified": tweet["user"]["verified"],
-                "lang": tweet["lang"],
-                "retweet_count": tweet["retweet_count"],
-                "favorite_count": tweet["favorite_count"]
-            }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-            # Produce the tweet to Kafka
-            avro_producer.produce(topic=topic, value=value)
-            avro_producer.flush()
+    public String getMessage() {
+        return message;
+    }
 
-            # Print the tweet
-            print(value)
-
-        except Exception as e:
-            print("Error: " + str(e))
-            return False
-
-        return True
-
-    def on_error(self, status):
-        print(status)
-        return False
-
-# Create a tweepy stream
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-listener = TweetListener()
-stream = tweepy.Stream(auth, listener)
-
-# Filter the stream for tweets containing the keyword 'kafka'
-stream.filter(track=['kafka'])
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
