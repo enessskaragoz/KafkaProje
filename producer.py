b7a1597b8d0abea243fb5a2b7de22eea3c26afb6
Enes <59286704+enessskaragoz@users.noreply.github.com>
2023-12-20 11:24:21 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,23 +1,13 @@
 from kafka import KafkaProducer
 import json
 import time
-import logging
 
-logger = logging.getLogger('producer')
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-logger.addHandler(ch)
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-try:
-    producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-    
-    for i in range(10):
-        data = {'message': f'Message {i}'}
-        producer.send('test-topic', value=data)
-        logger.info(f"Sent: {data}")
-        time.sleep(1)
+for i in range(10):
+    data = {'message': f'Message {i}'}
+    producer.send('test-topic', value=data)
+    print(f"Sent: {data}")
+    time.sleep(1)
 
-    producer.close()
-except Exception as e:
-    logger.error(f"Error: {str(e)}")
+producer.close()
