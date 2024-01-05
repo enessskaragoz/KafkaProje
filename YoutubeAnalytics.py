9e0672f380262be46b7657b67a6de34860bcc198
Enes <59286704+enessskaragoz@users.noreply.github.com>
2024-01-05 13:26:11 +0300

    Update YoutubeAnalytics.py

--- YoutubeAnalytics.py
+++ YoutubeAnalytics.py
@@ -52,8 +52,8 @@ if __name__ == "__main__":
                 "https://www.googleapis.com/youtube/v3/videos",
                 {'id': video_id, 'part': 'snippet,statistics'},
                 None):
-            # logging.info("Video here => %s", pprint(format_response(video)))
+
             producer.send('youtube_videos', json.dumps(format_response(video)).encode('utf-8'),
                           key=video_id.encode('utf-8'))
             print('Sent ', video['snippet']['title'])
-            # producer.flush()
+
